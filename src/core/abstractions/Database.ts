b8c695524035deec1a7ed38d9db3fc6b548c828c
ajoscram <ajoscram@gmail.com>
2021-06-28 11:22:17 -0600

    Removed the getSummoner method from the Database

--- src/core/abstractions/Database.ts
+++ src/core/abstractions/Database.ts
@@ -3,8 +3,7 @@ import { CompletedMatch, DiscordUser, OngoingMatch, Player, Summoner, SummonerOv
 export interface Database{
     getPlayer(user: DiscordUser): Player;
     getPlayers(users: DiscordUser[]): Player[];
-    getSummoner(summonerName: string): Summoner;
-    getSummonerOverallStats(sumomner: Summoner): SummonerOverallStats;
+    getSummonerOverallStats(summoner: Summoner): SummonerOverallStats;
     upsert(player: Player): void;
     insert(ongoingMatch: OngoingMatch): void;
     insert(completedMatch: CompletedMatch): void;
--- src/core/abstractions/SummonerFetcher.ts
+++ src/core/abstractions/SummonerFetcher.ts
@@ -1,5 +1,5 @@
 import { Summoner } from "../model";
 
 export interface SummonerFetcher{
-    getSummoner(summonerName: string): Summoner;
+    getSummoner(name: string): Summoner;
 }
