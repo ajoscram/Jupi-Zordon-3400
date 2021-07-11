f12c6d8b747a180f140488dc6234fbef312cc444
Alejandro Schmidt <ajoscram@gmail.com>
2021-07-11 00:46:28 -0600

    Changed Logger to an abstract singleton-like class

--- src/core/concretions/Context.ts
+++ src/core/concretions/Context.ts
@@ -1,8 +1,6 @@
 import { Predictor, Database, Message, Server, MatchFetcher, SummonerFetcher } from '../abstractions';
-import { Logger } from './logging';
 
 export interface Context{
-    readonly logger: Logger;
     readonly summonerFetcher: SummonerFetcher,
     readonly matchFetcher: MatchFetcher,
     readonly predictor: Predictor,
--- src/core/concretions/logging/Logger.ts
+++ src/core/concretions/logging/Logger.ts
@@ -1,26 +1,26 @@
 import { Writer } from "../../abstractions";
 
-export class Logger implements Writer{
-    private writers: Writer[] = [];
+export abstract class Logger{
+    private static writers: Writer[] = [];
 
-    add(writer: Writer): void{
-        this.writers.push(writer);
+    public static add(writer: Writer): void{
+        Logger.writers.push(writer);
     }
-
-    logInformation(text: string): void {
-        this.writers.forEach(
+    
+    public static logInformation(text: string): void {
+        Logger.writers.forEach(
             writer => writer.logInformation(text)
         );
     }
-
-    logWarning(text: string): void {
-        this.writers.forEach(
+    
+    public static logWarning(text: string): void {
+        Logger.writers.forEach(
             writer => writer.logWarning(text)
         );
     }
-
-    logError(text: string): void {
-        this.writers.forEach(
+    
+    public static logError(text: string): void {
+        Logger.writers.forEach(
             writer => writer.logError(text)
         );
     }
