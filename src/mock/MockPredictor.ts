a9db95c3180e1705249ff1aefc3226d17583f42b
Kazzak <ricardogago48@gmail.com>
2021-07-21 00:36:13 -0600

    Adding MockPredictor

--- src/mock/MockPredictor.ts
+++ src/mock/MockPredictor.ts
@@ -3,12 +3,17 @@ import { AIModel, Account, OngoingMatch, Prediction } from "src/core/model";
 
 export class MockPredictor implements Predictor {
     public async initialize(model: AIModel): Promise<void> {
-        throw new Error("Method not implemented.");
+        //Here will be load the model in the real predictor
     }
-    public async balance(players: Account[]): Promise<[Account[], Account[]]> {
-        throw new Error("Method not implemented.");
+    public async balance(players: Account[]): Promise<[Account[], Account[]]> { 
+        const teamSplitter: number = Math.floor( players.length/2 ); 
+        return [players.slice(0,teamSplitter),players.slice(teamSplitter)];
     }
     public async predict(ongoingMatch: OngoingMatch): Promise<Prediction> {
-        throw new Error("Method not implemented.");
+        return {
+            match: ongoingMatch,
+            probabilityBlueWins: 0.5,
+            probabilityRedWins: 0.5
+        };
     }
 }
